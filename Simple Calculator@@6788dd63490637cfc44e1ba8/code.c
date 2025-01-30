9743b635c7fa8806fbd414915f0b8222770a5fe9
Deepika-s04 <deepika0119.becse24@chitkara.edu.in>
2025-01-30 22:05:07 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -12,7 +12,11 @@ int main() {
     }else if(oper == '*'){
         printf("%d",num1*num2);
     }else if(oper == '/'){
-        printf("%d",num1/num2);
+        if(num2 == 0){
+            printf("Error:Division by zero");
+        }else{
+            printf("%d",num1/num2);
+        }
     }else{
         printf("Error");
     }
