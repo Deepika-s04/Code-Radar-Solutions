941a03e026734f84974308760cbbac22d4cfcc9f
Deepika-s04 <deepika0119.becse24@chitkara.edu.in>
2025-02-03 13:54:34 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -2,8 +2,7 @@
 int main() {
     int num1,num2;
     char oper;
-    scanf("%d %d ",&num1,&num2);
-    scanf(" %c",&oper);
+    scanf("%d %d %c",&num1,&num2,&oper);
 
     if(oper == '+'){
         printf("%d",num1+num2);
@@ -12,10 +11,10 @@ int main() {
     }else if(oper == '*'){
         printf("%d",num1*num2);
     }else if(oper == '/'){
-        if(num2 == 0){
-            printf("Error:Division by zero");
-        }else{
+        if(num2 != 0){
             printf("%d",num1/num2);
+        }else{
+            printf("error");
         }
     }else{
         printf("Error: Invalid operator");
