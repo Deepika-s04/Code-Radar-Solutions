b9524d1997fda556df1e42240a221ba999181028
Deepika-s04 <deepika0119.becse24@chitkara.edu.in>
2025-03-29 18:45:20 +0530

    Solution to Find the Median of an Array

--- Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
+++ Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
@@ -1,33 +1,40 @@
-// Your code here...
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+
+int main() {
     int size;
-    scanf("%d",&size);
+    scanf("%d", &size);
+
+    if (size <= 0) {
+
+        return 1;
+    }
 
     int arr[size];
 
-    for(int i = 0;i<size;i++){
-        scanf("%d",&arr[i]);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
     }
 
-    int temp;
-
-    for(int i =0;i<size;i++){
-        for(int j =i+1;j<size;j++){
-            if(arr[i]>arr[j]){
-                temp = arr[i];
+    // Sorting using Bubble Sort (not efficient but works for small input sizes)
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] > arr[j]) {
+                // Swap
+                int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
     }
 
-    int mid = size/2;
+    // Correct median calculation
+    int mid = size / 2;
 
-    if(size%2==0){
-        printf("%d",arr[mid-1]);
-    }else{
-        printf("%d",arr[mid]);
+    if (size % 2 == 0) {
+        printf("%d\n", arr[mid - 1]);  // First middle element
+    } else {
+        printf("%d\n", arr[mid]);  // Middle element
     }
+
     return 0;
 }
