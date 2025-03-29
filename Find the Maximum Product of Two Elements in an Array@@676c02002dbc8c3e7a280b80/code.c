41049240b7a3b2a1e555d866223174e7906244f0
Deepika-s04 <deepika0119.becse24@chitkara.edu.in>
2025-03-29 14:15:18 +0530

    Solution to Find the Maximum Product of Two Elements in an Array

--- Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
+++ Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
@@ -1,24 +1,54 @@
-#include<stdio.h>
-#include<limits.h>
+// #include<stdio.h>
+// #include<limits.h>
 
-int main() {
-    int size, num;
-    scanf("%d", &size);
+// int main() {
+//     int size, num;
+//     scanf("%d", &size);
     
-    if (size < 2) return 0;
+//     if (size < 2) return 0;
 
-    int firstmax = INT_MIN, secondmax = INT_MIN;
+//     int firstmax = INT_MIN, secondmax = INT_MIN;
 
-    for (int i = 0; i < size; i++) {
-        scanf("%d", &num);
-        if (num > firstmax) {
-            secondmax = firstmax;
-            firstmax = num;
-        } else if (num > secondmax && num != firstmax) {
-            secondmax = num;
+//     for (int i = 0; i < size; i++) {
+//         scanf("%d", &num);
+//         if (num > firstmax) {
+//             secondmax = firstmax;
+//             firstmax = num;
+//         } else if (num > secondmax && num != firstmax) {
+//             secondmax = num;
+//         }
+//     }
+
+//     printf("%d\n", (firstmax - 1) * (secondmax - 1));
+//     return 0;
+// }
+
+#include<stdio.h>
+int main(){
+    int size;
+    scanf("%d",&size);
+
+    if(size<2){
+        return 0;
+    }
+
+    int arr[size];
+
+    for(int i = 0; i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    int a = arr[0];
+    int b = arr[1];
+    for(int i = 0;i<size;i++){
+        if(arr[i]*arr[i+1] > a*b){
+            a = arr[i];
+            b = arr[j];
         }
     }
 
-    printf("%d\n", (firstmax - 1) * (secondmax - 1));
-    return 0;
+    printf("%d\n",a*b);
+
+
+
 }
