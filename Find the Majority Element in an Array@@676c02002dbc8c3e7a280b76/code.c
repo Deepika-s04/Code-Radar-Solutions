373becc9386674aa0e363fea1e8979bf4df2aec5
Deepika-s04 <deepika0119.becse24@chitkara.edu.in>
2025-03-29 11:44:25 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -1,65 +1,35 @@
-// Your code here...
-#include<stdio.h>
-int main(){
+
+
+#include <stdio.h>
+
+int main() {
     int size;
-    scanf("%d",&size);
+    scanf("%d", &size);
 
     int arr[size];
 
-    for(int i = 0; i< size; i++){
-        scanf("%d",&arr[i]);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
     }
 
-    int count = 0;
-    int majority = size/2;
-
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
-            if(arr[i] == arr[j]){
+    // Checking for majority element
+    for (int i = 0; i < size; i++) {
+        int count = 0;
+        
+        // Count occurrences of arr[i]
+        for (int j = 0; j < size; j++) {
+            if (arr[j] == arr[i]) {
                 count++;
             }
         }
 
-        if(count > size/2){
-            printf("%d",arr[i]);
-        } else{
-              printf("-1\n");
-              } 
+        // Check if count is greater than half of the size
+        if (count > size / 2) {
+            printf("%d\n", arr[i]);
+            return 0; // Stop the program as we found the answer
         }
-        return 0;
-  
+    }
+
+    printf("-1\n"); // If no majority element found
+    return 0;
 }
-
-// #include <stdio.h>
-
-// int main() {
-//     int size;
-//     scanf("%d", &size);
-
-//     int arr[size];
-
-//     for (int i = 0; i < size; i++) {
-//         scanf("%d", &arr[i]);
-//     }
-
-//     // Checking for majority element
-//     for (int i = 0; i < size; i++) {
-//         int count = 0;
-        
-//         // Count occurrences of arr[i]
-//         for (int j = 0; j < size; j++) {
-//             if (arr[j] == arr[i]) {
-//                 count++;
-//             }
-//         }
-
-//         // Check if count is greater than half of the size
-//         if (count > size / 2) {
-//             printf("%d\n", arr[i]);
-//             return 0; // Stop the program as we found the answer
-//         }
-//     }
-
-//     printf("-1\n"); // If no majority element found
-//     return 0;
-// }
