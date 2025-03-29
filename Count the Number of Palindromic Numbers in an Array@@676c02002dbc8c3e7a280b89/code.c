a5213a38d66d7cef776ad37f6b92484ee0fd97bd
Deepika-s04 <deepika0119.becse24@chitkara.edu.in>
2025-03-29 11:01:55 +0530

    Solution to Count the Number of Palindromic Numbers in an Array

--- Count the Number of Palindromic Numbers in an Array@@676c02002dbc8c3e7a280b89/code.c
+++ Count the Number of Palindromic Numbers in an Array@@676c02002dbc8c3e7a280b89/code.c
@@ -1,24 +1,57 @@
-#include<stdio.h>
-int main(){
-    int n;
-    scanf("%d",&n);
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int a[n];
+//     int count = 0;
+//     for(int i = 0;i<n;i++){
+//         scanf("%d",&a[i]);
+//     }
+//     for(int i = 0;i<n;i++){
+//         int num = a[i];reverse=0;digit;
+//         while(num>0){
+//             digit = num%10;
+//             reverse = reverse*10+num;
+//             num = num/10;
+//         }
+
+//         if(a[i] == reverse){
+//             count++;
+//         }
+//     }
+//     printf("%d",count);
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+    
+    scanf("%d", &n);
     int a[n];
-    int count = 0;
-    for(int i = 0;i<n;i++){
-        scanf("%d",&a[i]);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
     }
-    for(int i = 0;i<n;i++){
-        int num = a[i];reverse=0;digit;
-        while(num>0){
-            digit = num%10;
-            reverse = reverse*10+num;
-            num = num/10;
+
+    for (int i = 0; i < n; i++) {
+        int num = a[i], reversed = 0, digit;
+
+        // Reverse the number
+        while (num > 0) {
+            digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
         }
 
-        if(a[i] == reverse){
+        // Check if it's a palindrome
+        if (a[i] == reversed) {
             count++;
         }
     }
-    printf("%d",count);
+
+    printf("%d\n", count);
+
     return 0;
 }
